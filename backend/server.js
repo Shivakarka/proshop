0e039fa44e32ce03d6e95eae20ee037410145dad
shivakarka <shivakarka@gmail.com>
2023-08-04 20:50:08 +0530

    initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import products from "./data/products.js";
+
+const port = 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.send(product);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
