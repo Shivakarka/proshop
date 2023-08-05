d9788a951738e24b3e39f05c20d0186ef6e2a379
shivakarka <shivakarka@gmail.com>
2023-08-05 14:29:21 +0530

    created productRoutes and asyncHandler

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -14,14 +14,7 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.get("/api/products", (req, res) => {
-  res.send(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.send(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
