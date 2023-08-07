8dcd073510fcca1222e6a57e89b54081da4159b7
shivakarka <shivakarka@gmail.com>
2023-08-07 23:52:28 +0530

    added register controller/endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,6 +33,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Middleware to hash password before saving to database
+userSchema.pre("save", async function (next) {
+  // Only hash password if password is modified
+  if (!this.isModified("password")) {
+    next();
+  } else {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
