10c29bb6b4c31d613ef56bde74bbe8e437e617b1
shivakarka <shivakarka@gmail.com>
2023-08-14 16:11:46 +0530

    small bug fix

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,6 +1,6 @@
 import { LinkContainer } from "react-router-bootstrap";
 import { Table, Button, Row, Col } from "react-bootstrap";
-import { FaEdit, FaTrash } from "react-icons/fa";
+import { FaEdit, FaTrash, FaPlus } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import {
   useGetProductsQuery,
@@ -14,35 +14,35 @@ import { toast } from "react-toastify";
 
 const ProductListScreen = () => {
   const { pageNumber } = useParams();
+
   const { data, isLoading, error, refetch } = useGetProductsQuery({
     pageNumber,
   });
-
-  const [createProduct, { isLoading: loadingCreate }] =
-    useCreateProductMutation();
 
   const [deleteProduct, { isLoading: loadingDelete }] =
     useDeleteProductMutation();
 
   const deleteHandler = async (id) => {
-    if (window.confirm("Are you sure you want to delete this product?")) {
+    if (window.confirm("Are you sure")) {
       try {
         await deleteProduct(id);
-        toast.success("Product deleted");
         refetch();
-      } catch (error) {
-        toast.error(error?.data?.message || error?.error);
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
       }
     }
   };
 
+  const [createProduct, { isLoading: loadingCreate }] =
+    useCreateProductMutation();
+
   const createProductHandler = async () => {
-    if (window.confirm("Are you sure you want to create new product?")) {
+    if (window.confirm("Are you sure you want to create a new product?")) {
       try {
         await createProduct();
         refetch();
-      } catch (error) {
-        toast.error(error?.data?.message || error?.error);
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
       }
     }
   };
@@ -54,59 +54,60 @@ const ProductListScreen = () => {
           <h1>Products</h1>
         </Col>
         <Col className="text-end">
-          <Button className="btn-sm m-3" onClick={createProductHandler}>
-            <FaEdit /> Create Product
+          <Button className="my-3" onClick={createProductHandler}>
+            <FaPlus /> Create Product
           </Button>
         </Col>
       </Row>
 
       {loadingCreate && <Loader />}
       {loadingDelete && <Loader />}
-
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant="danger">{error}</Message>
+        <Message variant="danger">{error.data.message}</Message>
       ) : (
-        <Table striped hover responsive className="table-sm">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>NAME</th>
-              <th>PRICE</th>
-              <th>CATEGORY</th>
-              <th>BRAND</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.products?.map((product) => (
-              <tr key={product._id}>
-                <td>{product._id}</td>
-                <td>{product.name}</td>
-                <td>${product.price}</td>
-                <td>{product.category}</td>
-                <td>{product.brand}</td>
-                <td>
-                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                    <Button className="btn-sm mx-2" variant="light">
-                      <FaEdit />
-                    </Button>
-                  </LinkContainer>
-                  <Button
-                    className="btn-sm"
-                    variant="danger"
-                    onClick={() => deleteHandler(product._id)}
-                  >
-                    <FaTrash style={{ color: "white" }} />
-                  </Button>
-                </td>
+        <>
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {data.products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                      <Button variant="light" className="btn-sm mx-2">
+                        <FaEdit />
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteHandler(product._id)}
+                    >
+                      <FaTrash style={{ color: "white" }} />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Paginate pages={data.pages} page={data.page} isAdmin={true} />
+        </>
       )}
-      <Paginate pages={data.pages} pageNumber={data.pageNumber} isAdmin />
     </>
   );
 };
